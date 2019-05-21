2ffa2a24c842c527729624ba9dfaf06eb571585f
csabat123 <38679522+csabat123@users.noreply.github.com>
2019-05-21 12:16:54 +1000

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,65 @@
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('home');
 }) ->name('Login');
 
 Route::get('formwrap', function () {
-    return view('index');
+    return view('studentfrontend.studentindex');
 });
 
-Route::get('navbar', array('as' => 'navbtn', function()
-{
-    return view('login');
-}));
+Route::get('studentupdate', function () {
+    return view('studentfrontend.update');
+});
+
+Route::get('studentindex', function () {
+    return view('studentfrontend.studentindex');
+});
 
 
 
-// Route::get('menu-toggle', 'TestController@navbar');
+Route::get('studentgrievance', function () {
+    return view('studentfrontend.grievance');
+});
+
+Route::get('admingrievance', function () {
+    return view('adminfrontend.gu');
+});
+
+Route::get('adminstory', function () {
+    return view('adminfrontend.adminstory');
+});
+
+Route::get('adminteam', function () {
+    return view('adminfrontend.team');
+});
+
+Route::get('admin', function () {
+    return view('adminfrontend.adminstory');
+});
+
+Route::get('teamedit', function () {
+    return view('adminfrontend.teamedit');
+});
+Route::resource('tasks', 'TaskController');
 
 
 
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('studentfrontend.studentindex');
+
+
+Route::get('/admin', 'AdminController@index') ;
+return redirect("studentfrontend.studentindex");
+
+Route::get('/student', 'StudentController@index');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
